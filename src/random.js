63dd605be97b61d2e2d1a162844e6b89572cc2d7
sheerblack <mclearyd@yahoo.com>
2020-01-09 16:13:47 +0000

    JS Lint complained about some stuff

--- src/random.js
+++ src/random.js
@@ -4,41 +4,43 @@ const SHUFFLE = 400;
 
 function generateRandom(maxnum, maxchoice)
 {
-    
+
     var elements = [];
     var choices = [];
-    
+
     if (maxnum > maxchoice) {
-        // load all the numbers into an array so that we can start shuffling them for randomness
-        for (i=0; i<maxnum; i++){
+        // load all the numbers into an array so that we
+        // can start shuffling them for randomness
+        for(i=0; i<maxnum; i++){
             elements.push(i+1);
         }
-        
+
         // Shuffle the deck
-        for (i=0; i<SHUFFLE; i++){
+        for(i=0; i<SHUFFLE; i += 1){
             selected = Math.floor(Math.random() * elements.length);
             movevar = elements[selected];
             elements.splice(selected, 1);
             elements.push(movevar);
         }
-        
+
         // Select the numbers from the array
-        for (i=0; i<maxchoice; i++){
+        for (i=0; i<maxchoice; i += 1){
             choice = Math.floor(Math.random() * elements.length);
             choices.push(elements[choice]);
             elements.splice(choice, 1);
         }
-    
+
         // Sort the results and then display them
         choices.sort(function(a, b){return a - b});
     }
     return choices;
-    
+
 }
+
 function displayRandom(maxnum, maxchoices, outputlocation)
 {
     var randomChoices = generateRandom(maxnum, maxchoices);
-    
+
     var output = "<p>Number of items in array = " + maxnum + "</p>";
     output = output + "<p>Number of choices = " + maxchoices + "</p>";
     output = output + randomChoices;
